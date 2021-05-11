837104c0681ac009fa88727a81d819bb9c47e121
holgerino <holgerino@allbut.social>
2021-05-12 01:30:41 +0200

    🚧 ✨ qr code on login page

--- plugin.php
+++ plugin.php
@@ -8,6 +8,23 @@ class InstalogIn
 {
     public function __construct()
     {
+        // wp_enqueue_style('instalog-in-global', plugin_dir_url(__FILE__) . 'style/style.css?v=1');
+        // TODO: style on admin pages
+        $this->login_page();
+    }
+
+    private function login_page()
+    {
+        add_action('login_head', function () {
+            wp_enqueue_style('instalog-in-login', plugin_dir_url(__FILE__) . 'style/login.css?v=1');
+        });
+
+        // TODO login_footer fuer js
+
+        add_action('login_footer', function () {
+            ?> <script async id="instalogin-js" src="https://cdn.instalog.in/js/instalogin.js"></script> <?php
+            wp_enqueue_script('instalog-in-qr-widget', plugin_dir_url(__FILE__) . 'scripts/login.js?v=1');
+        });
     }
 }
 
---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+/** @type {HTMLButtonElement} */
+const add_after_element = document.querySelector("p.submit");
+
+if (add_after_element) {
+  const div = document.createElement("div");
+  div.id = "instalogin";
+  add_after_element.after(div);
+
+  document
+    .getElementById("instalogin-js")
+    .addEventListener("load", function () {
+      new Instalogin.Auth({
+        key: "VluObzy1BoNiFcgm5OXSQun42pF9pFNx", // TODO: use setting variable
+        authenticationUrl: "/path/to/login-controller", // The authentication controller to process the authentication
+      }).start();
+    });
+} else {
+  console.error(
+    "instalog.in - could not find Button `Log In`. Aborted adding QR code."
+  );
+}
