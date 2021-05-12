f3503ee0f12deecefaabb633e4ba9d7165357f25
holgerino <holgerino@allbut.social>
2021-05-12 22:22:23 +0200

    ✨ 🥅 sendMail use settings and catch errors

--- send_mail.php
+++ send_mail.php
@@ -1,31 +1,54 @@
 <?php
-
-
-require __DIR__ . '/vendor/autoload.php';
-$client = new \Instalogin\Client('VluObzy1BoNiFcgm5OXSQun42pF9pFNx', '7e2672af946831902319d3a17573bac3ed3897be1eb3c962b074bd62e75293cb');
-
-// wp_remote_get https://developer.wordpress.org/reference/functions/wp_remote_get/
-
 include_once '../../../wp-blog-header.php';
+require __DIR__ . '/vendor/autoload.php';
 
-try {
-    // TODO error handling:
-    $user = wp_get_current_user();
-    $client->provisionIdentity($user->user_email, array(
-        'sendEmail' => true // Let Instalogin handle the mail sending
-    ));
-    
-    $url = $_GET['redirect']; ?> 
-    <div>
-        Sending activation email... You should be redirected automatically in a few seconds.
-        <br>
-        If not click <a href="<?=$url?>">here</a>.
-    </div>
-    <script> 
-        window.location.href="<?=$url?>?insta-sent=true";
-    </script>
-    <?php
-} catch (\Instalogin\Exception\TransportException $e) {
-        echo 'Could not connect to Instalogin service: '.$e->getMessage();
-        // TODO: return with error message
+class InstalogInSendMail
+{
+    public function __construct()
+    {
+        $api_enabled = get_option('instalog-in-api-enabled');
+
+        if ($api_enabled != 1) {
+            echo "Instalog.in API has been disabled by an administrator.";
+            return;
+        }
+
+        $api_key = get_option('instalog-in-api-key');
+        $api_secret = get_option('instalog-in-api-secret');
+
+        if ($api_key == false || $api_secret == false) {
+            echo "Instalog.in API key or secret missing. Please contact a site administrator for help.";
+            return;
+        }
+
+        // TODO: does not throw on error
+        $client = new \Instalogin\Client($api_key, $api_secret);
+        
+        // Fails if user is not logged in
+        $user = wp_get_current_user();
+        // send user back to previous page
+        $url = $_GET['redirect'];
+        
+        try {
+            $client->provisionIdentity($user->user_email, array(
+                'sendEmail' => true // Let Instalogin handle the mail sending
+            ));
+        } catch (\Instalogin\Exception\TransportException $e) {
+            echo 'Could not connect to Instalogin service: '.$e->getMessage();
+            echo '<br><a href="<?=$url?>">Go back.</a>';
+            return;
+        } ?>
+            
+        <div>
+            Sending activation email... You should be redirected automatically in a few seconds.
+            <br>
+            If not click <a href="<?=$url?>">here</a>.
+        </div>
+        <script> 
+            window.location.href="<?=$url?>?insta-sent=true";
+        </script>
+        <?php
     }
+}
+
+$instalog_in_send_mail = new InstalogInSendMail();
