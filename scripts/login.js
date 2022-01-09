de563a3a085940402d712cf64aa8d204588c73ba
holgerino <holgerino@allbut.social>
2022-01-09 21:37:34 +0100

    ✨ stayonpage redirect for insta codes

--- scripts/login.js
+++ scripts/login.js
@@ -31,15 +31,13 @@ function init_insta(id, api_key, display_type, redirect) {
 
   let url = insta_api + "login-controller";
   console.log(redirect);
-  if(redirect && redirect != '') {
-
-    if(redirect == 'stayonpage') {
+  if (redirect && redirect != "") {
+    if (redirect == "stayonpage") {
       redirect = window.location.href;
     }
 
-    url += '?redirect=' + redirect;
+    url += "?redirect=" + redirect;
   }
-
 
   // instance may be used by other scripts
   return new Instalogin.Auth({
